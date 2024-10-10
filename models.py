320ac9e6e1e9fd64a370878b886950deaef0da49
JRivera <24005123@galileo.edu>
2024-10-09 20:33:13 -0600

    Cambios a models

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
 import sys
 
 def rf():
-    print("Ejecutando Modelo")
+    print("Ejecutando Random Forest")
+
 
 def main():
-    if sys.argv[1] == 'rf':
-        rf()
-    else:
-        print("Sin modelo")
-
+   
+   if sys.argv[1] == 'rf':
+       rf()
+   else:
+       print("Sin modelo")
+if __name__ == '__main__':
+    main()
